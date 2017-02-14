8defcfb8d447652598fef1d947fdb1cdc66a22cb
Rohan Isaac <rohan_isaac@yahoo.com>
2017-02-14 09:56:51 -0500

    completely untested, added scheduler

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # i2c-temp-humidity
-A simple I2C temperature humidity sensor readout in python for use on a Raspberry Pi
+A simple I2C temperature humidity sensor readout in python for use on a
+Raspberry Pi
 
 # Sensor
-The HIH8120-021-001 is a Digital Humidity/Temperature Sensor are digital output-type relative humidity (RH) and temperature sensors combined in the same package. 
+The HIH8120-021-001 is a Digital Humidity/Temperature Sensor are digital
+output-type relative humidity (RH) and temperature sensors combined in the same
+package. 
 
 The output of this sensor is I2C.
 
-The sensor I used was purchased form [Newark](http://www.newark.com/honeywell/hih8120-021-001/humidity-digital-2-rh-sip-4/dp/04X4284) for about $9.
+The sensor I used was purchased form
+[Newark](http://www.newark.com/honeywell/hih8120-021-001/humidity-digital-2-rh-sip-4/dp/04X4284)
+for about $9.
+
+Collect and send to graphite,
+Visualize with grafana
--- getTempHumidity.py
+++ getTempHumidity.py
@@ -1,69 +1,55 @@
+from __future__ import print_function
+import argparse
 import smbus
-import time
-import datetime
 import sys
-import subprocess
+from apsheduler.schedulers.blocking import BlockingScheduler
 
-def main():
-	bus = smbus.SMBus(1)  # i2c port 1
+def setup(i2c_port=1):
+    bus = smbus.SMBus(i2c_port)
 
-	try:
-		bus.write_quick(0x27)
-	except IOError, e:
-		#print e.errno
-		#print e
-		print "The device is not present. Check that it is plugged in correctly."
-		makeWebPage(0,0,0)
-		return [0,0]
+    try:
+        bus.write_quick(0x27)
+    except IOError as e:
+        print("The device is not present. Check that it is plugged in correctly.")
 
-	time.sleep(0.1)			# give us a moment to take the data
-	try:
-		vals =  bus.read_i2c_block_data(0x27,0x0,4)
-	except IOError, e:
-		print "The device was probably unplugged? Check connections."
-		makeWebPage(0,0,0)
-		return [0,0]
+    # give us a moment to take the data
+    time.sleep(0.1)
 
-	ts = datetime.datetime.utcnow()
 
-	# data bytes as defined in datasheet
-	db1 = vals[0]&0xFF
-	db2 = vals[1]&0xFF
-	db3 = vals[2]&0xFF
-	db4 = vals[3]&0xFF
+def take_data():
+    try:
+        vals = bus.read_i2c_block_data(0x27, 0x0, 4)
+        # extract data
+        # data bytes as defined in datasheet
+        db1 = vals[0] & 0xFF
+        db2 = vals[1] & 0xFF
+        db3 = vals[2] & 0xFF
+        db4 = vals[3] & 0xFF
 
-	status = (db1&0xC0) >> 6
-	humidityRaw = db2 + ((db1&0x3F)<<8)
-	tempRaw = (db3<<6) + ((db4>>2)&0x3F)
+        status = (db1 & 0xC0) >> 6
+        humidityRaw = db2 + ((db1 & 0x3F) << 8)
+        tempRaw = (db3 << 6) + ((db4 >> 2) & 0x3F)
 
-	humidity = (humidityRaw/16383.0)*100
-	tempC = (165*(tempRaw/(16383.0))) - 40
-	tempF = (tempC*9/5)+32
+        humidity = (humidityRaw / 16383.0) * 100
+        tempC = (165 * (tempRaw / (16383.0))) - 40
+        tempF = (tempC * 9 / 5) + 32
 
-	makeWebPage(ts,humidity, tempF)
-#	print "temp: %03.3f, humidity: %03.3f" % (tempF, humidity)
-	return [humidity,tempF]
+    except IOError as e:
+        print("The device was probably unplugged? Check connections.")
 
-def makeWebPage(ts,humidity,tempF):
-	#fh = open('/home/pi/Documents/i2c_humidity/status.html','w')
-	fh = open('/var/www/html/index.html','w')
-	fh.write("<!DOCTYPE html>\n")
-	fh.write('<html lang="en">\n')
-	fh.write('	<head>\n')
-	fh.write('		<meta charset="utf-8">\n')
-	fh.write('		<meta http-equiv="refresh" content="5">\n')
-	fh.write('		<title>Sensor Values</title>\n')
-	fh.write('	</head\n>')
-	fh.write('	<body>\n')
-	fh.write("time: " + str(ts) + '<br>\n')
-	fh.write("humidity: %.4f<br>\n" % (humidity))
-	fh.write("temp: %.4f<br>\n" % (tempF))
-	fh.write('	</body>\n')
-	fh.write('</html>\n')
+def parse_args():
+    parser = argparse.ArgumentParser(description="Get temperature and humidity")
+    parser.add_argument('--port', type=int, required=False, default=1,
+            help='i2c port of temp/humidity sensor')
 
-	fh.close()
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
-	[h,tF] = main()
-	st = "temp: " + str(tF) + " humidity: " + str(h)
-	sys.stdout.write(st)
+    args = parse_args()
+    setup(args.port)
+
+    # setup the job on a schedule of once every minute
+    sched = BlockingScheduler()
+    sched.add_job(take_data, 'interval', minutes=1)
+    sched.start()

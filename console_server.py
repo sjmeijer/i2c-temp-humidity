eb161c7dff9dbe095739b1a983b5a680c6f06d06
Rohan Isaac <rohan_isaac@yahoo.com>
2017-03-21 22:39:29 -0400

    Added text server, better chance of working

---
+++ console_server.py
@@ -0,0 +1,50 @@
+"""
+Reads out data from the sensor and prints it to the terminal
+
+"""
+
+from __future__ import print_function, absolute_import
+from get_temp_humidity import setup, take_data
+import argparse
+from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import datetime
+
+
+def write_data(bus=None, test=False):
+    """
+    Take temp and humidity data from the sensor on `bus` and write it to the
+    console
+
+    Parameters
+    ----------
+    test: Use simulated data
+    """
+    h, tc, tf = take_data(bus, test)
+    print("Temperature(F): %0.2f, Humidity: %0.2f" % (tf, h))
+    return
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Get temperature and humidity")
+    parser.add_argument('--i2cport', type=int, required=False, default=1,
+                        help='i2c port of temp/humidity sensor')
+    parser.add_argument('--interval', type=int, required=False, default=1,
+                        help='interval (in minutes) to take data')
+    parser.add_argument('--test', type=bool, required=False, default=False,
+                        help='test mode, not real hardware connection')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    sched = BlockingScheduler()
+
+    if args.test:
+        sched.add_job(lambda: write_data(test=True),
+                      'interval', minutes=args.interval)
+    else:
+        bus = setup(args.i2cport)
+        sched.add_job(lambda: write_data(bus=bus),
+                      'interval', minutes=args.interval)
+
+    sched.start()

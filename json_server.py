7eabe0d148ba02fd8718df2f280dd69a53ce6c8c
Sam Meijer <samuelmeijer@gmail.com>
2017-03-31 08:51:18 -0600

    Added support for input files to select readout

--- json_server.py
+++ json_server.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from devices.TC74_device import TC74
 from devices.HIH8120 import HIH8120
 
-def write_data(device=None):
+def write_data(devs):
 	
 	text = []
 	
@@ -40,6 +40,11 @@ def parse_args():
 	parser.add_argument('--interval', type=int, required=False, default=1,
                         help='interval (in seconds) to take data')
 
+	parser.add_argument('--description', type=str, required=False, nargs='*',
+						help='device description [--description tc74 0 "Cleanroom temperature"]')
+
+	parser.add_argument("--f", dest="filename", required=False,
+                    help="input file describing hardware", metavar="FILE")
 
 	return parser.parse_args()
 
@@ -50,6 +55,9 @@ if __name__ == "__main__":
 
 	devs = []
 
+	if(args.description):
+		print(description)	
+
 	if(args.tc74):
 		for dd in args.tc74:
 			print("Adding a TC74 device on port ",dd)
@@ -58,11 +66,33 @@ if __name__ == "__main__":
 		for dd in args.hih8120:
 			devs.append(HIH8120(dd))
 
+	if(args.filename):
+		with open(args.filename,'r') as fh:
+			fh.seek(0)
+			for line in fh:
+				if(len(line.split(',')) !=3):
+					print("File incorrectly formatted at line: ",line)
+						
+				else:
+					aDevice,aPort,aDescription = line.split(',')
+					aPort = int(aPort)
+					
+					try:
+						exec("d = %s(%d)" % (aDevice,aPort))
+						devs.append(d)
+						d.description = aDescription.rstrip()
+						
+					except Exception as e:
+						print(e)
+
 	if(len(devs) == 0):
 		print("You must specify some devices to read out. Quitting...")
 		raise SystemExit(0)
 
-	sched.add_job(lambda: write_data(device=devs),
+
+
+
+	sched.add_job(lambda: write_data(devs=devs),
                  'interval', seconds=args.interval)
 	
 	print("Starting scheduler...")
